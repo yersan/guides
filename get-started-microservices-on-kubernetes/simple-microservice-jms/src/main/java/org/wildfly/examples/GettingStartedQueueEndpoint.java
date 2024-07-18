510df1d7852f88b848db1ac97d2cd61d747ea925
Emmanuel Hugonnet <ehugonne@redhat.com>
2024-07-18 15:48:04 +0200

    Simplifying the code and the pom.xml to avoid using a cli script.
    Signed-off-by: Emmanuel Hugonnet <ehugonne@redhat.com>

--- get-started-microservices-on-kubernetes/simple-microservice-jms/src/main/java/org/wildfly/examples/GettingStartedQueueEndpoint.java
+++ get-started-microservices-on-kubernetes/simple-microservice-jms/src/main/java/org/wildfly/examples/GettingStartedQueueEndpoint.java
@@ -2,8 +2,9 @@ package org.wildfly.examples;
 
 import jakarta.annotation.Resource;
 import jakarta.inject.Inject;
-import jakarta.jms.JMSConnectionFactory;
 import jakarta.jms.JMSContext;
+import jakarta.jms.JMSDestinationDefinition;
+import jakarta.jms.JMSDestinationDefinitions;
 import jakarta.jms.JMSException;
 import jakarta.jms.Queue;
 import jakarta.ws.rs.GET;
@@ -12,23 +13,31 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-
+@JMSDestinationDefinitions(
+    value = {
+        @JMSDestinationDefinition(
+            name = "java:/queue/gettingStartedQueue",
+            interfaceName = "jakarta.jms.Queue",
+            destinationName = "getting-started-queue",
+            properties = {"enable-amq1-prefix=false"}
+        )
+    }
+)
 @Path("/message")
-public class TestQueueEndpoint {
+public class GettingStartedQueueEndpoint {
 
-    @Resource(lookup="java:/jms/remote/testQueue")
-    Queue testQueue;
+    @Resource(lookup="java:/queue/gettingStartedQueue")
+    private Queue queue;
 
     @Inject
-    @JMSConnectionFactory("java:/jms/remote/activemqPCF")
     private JMSContext context;
 
     @GET
     @Path("/send")
     @Produces(MediaType.TEXT_PLAIN)
     public Response sendMessage(final @QueryParam("content") String content) throws JMSException {
-        String response = "Sent " + content + " to " + testQueue.getQueueName();
-        context.createProducer().send(testQueue, content);
+        String response = "Sent " + content + " to " + queue.getQueueName();
+        context.createProducer().send(queue, content);
         return Response.ok(response).build();
     }
 }
