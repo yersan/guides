ae845c8191a51ead81c4fdca0b7f52c1729fc819
Tommaso Borgato <tborgato@redhat.com>
2024-08-01 20:06:29 +0200

    Sample applications for wildfly-mini-serie - review artemis

---
+++ get-started-microservices-on-kubernetes/simple-microservice-jms/src/main/java/org/wildfly/examples/GettingStartedQueueEndpoint.java
@@ -0,0 +1,43 @@
+package org.wildfly.examples;
+
+import jakarta.annotation.Resource;
+import jakarta.inject.Inject;
+import jakarta.jms.JMSContext;
+import jakarta.jms.JMSDestinationDefinition;
+import jakarta.jms.JMSDestinationDefinitions;
+import jakarta.jms.JMSException;
+import jakarta.jms.Queue;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+@JMSDestinationDefinitions(
+		value = {
+				@JMSDestinationDefinition(
+						name = "java:/queue/gettingStartedQueue",
+						interfaceName = "jakarta.jms.Queue",
+						destinationName = "getting-started-queue",
+						properties = {"enable-amq1-prefix=false"}
+				)
+		}
+)
+@Path("/message")
+public class GettingStartedQueueEndpoint {
+
+	@Resource(lookup="java:/queue/gettingStartedQueue")
+	private Queue queue;
+
+	@Inject
+	private JMSContext context;
+
+	@GET
+	@Path("/send")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response sendMessage(final @QueryParam("content") String content) throws JMSException {
+		String response = "Sent " + content + " to " + queue.getQueueName();
+		context.createProducer().send(queue, content);
+		return Response.ok(response).build();
+	}
+}
---
+++ get-started-microservices-on-kubernetes/simple-microservice-jms/src/main/java/org/wildfly/examples/mdb/GettingStartedQueueMDB.java
@@ -0,0 +1,36 @@
+package org.wildfly.examples.mdb;
+
+import jakarta.ejb.ActivationConfigProperty;
+import jakarta.ejb.MessageDriven;
+import jakarta.jms.JMSException;
+import jakarta.jms.Message;
+import jakarta.jms.MessageListener;
+import jakarta.jms.TextMessage;
+
+import java.util.logging.Logger;
+
+@MessageDriven(
+		name = "GettingStartedQueueMDB",
+		activationConfig = {
+				@ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "queue/gettingStartedQueue"),
+				@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "jakarta.jms.Queue"),
+				@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge")}
+)
+public class GettingStartedQueueMDB implements MessageListener {
+
+	private static final Logger LOGGER = Logger.getLogger(GettingStartedQueueMDB.class.toString());
+
+	public void onMessage(Message rcvMessage) {
+		TextMessage msg = null;
+		try {
+			if (rcvMessage instanceof TextMessage) {
+				msg = (TextMessage) rcvMessage;
+				LOGGER.info("Received Message from queue: " + msg.getText());
+			} else {
+				LOGGER.warning("Message of wrong type: " + rcvMessage.getClass().getName());
+			}
+		} catch (JMSException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
